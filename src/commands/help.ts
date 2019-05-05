ccc3d11ef89087a61db93ae162556972032fabd0
Supamiu <contact@flavien-normand.fr>
2019-05-05 19:56:15 +0200

    chore: initial commit for bot structure

---
+++ src/commands/help.ts
@@ -0,0 +1,45 @@
+import {Command} from "./command";
+import {CommandContext} from "../models/command_context";
+import {config} from "../config/config";
+
+export class HelpCommand implements Command {
+    readonly commandNames = ["help", "halp", "hlep"];
+
+    private commands: Command[];
+
+    constructor(commands: Command[]) {
+        this.commands = commands;
+    }
+
+    async run(commandContext: CommandContext): Promise<void> {
+        const allowedCommands = this.commands.filter(command => command.hasPermissionToRun(commandContext));
+
+        if (commandContext.args.length == 0) {
+            // No command specified, give the user a list of all commands they can use.
+            const commandNames = allowedCommands.map(command => command.commandNames[0]);
+            await commandContext.originalMessage.reply(
+                `here is a list of commands you can run: ${commandNames.join(", ")}. Try ${config.prefix}help ${commandNames[0]} to learn more about one of them.`);
+            return;
+        }
+
+        const matchedCommand = this.commands.find(command => command.commandNames.includes(commandContext.args[0]));
+        if (!matchedCommand) {
+            await commandContext.originalMessage.reply("I don't know about that command :(. Try ${config.prefix}help to find all commands you can use.");
+            return Promise.reject("Unrecognized command");
+        } else if (allowedCommands.includes(matchedCommand)) {
+            await commandContext.originalMessage.reply(this.buildHelpMessageForCommand(matchedCommand, commandContext));
+        }
+    }
+
+    private buildHelpMessageForCommand(command: Command, context: CommandContext): string {
+        return `${command.getHelpMessage(context.commandPrefix)}\nCommand aliases: ${command.commandNames.join(", ")}`;
+    }
+
+    hasPermissionToRun(commandContext: CommandContext): boolean {
+        return true;
+    }
+
+    getHelpMessage(commandPrefix: string) {
+        return "I think you already know how to use this command...";
+    }
+}
---
+++ src/server.ts
@@ -0,0 +1,32 @@
+import Discord, { Message } from "discord.js";
+import { config, BotConfig } from "./config/config";
+import { CommandHandler } from "./command_handler";
+
+validateConfig(config);
+
+const commandHandler = new CommandHandler(config.prefix);
+
+const client = new Discord.Client();
+
+//https://discordapp.com/api/oauth2/authorize?client_id=574647725900169226&permissions=2112&scope=bot
+
+client.on("ready", () => {
+  console.log("Bot has started");
+});
+
+client.on("message", (message: Message) => {
+  commandHandler.handleMessage(message);
+});
+
+client.on("error", e => {
+  console.error("Discord client error!", e);
+});
+
+client.login(config.token);
+
+/** Pre-startup validation of the bot config. */
+function validateConfig(config: BotConfig) {
+  if (!config.token) {
+    throw new Error("You need to specify your Discord bot token!");
+  }
+}
