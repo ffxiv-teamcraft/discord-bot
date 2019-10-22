93f9970f132a9c6c1f61e43a33c7a97a2e3f44d6
Supamiu <contact@flavien-normand.fr>
2019-10-22 21:59:50 +0200

    chore: better implementation for db command fix

--- src/commands/database-command.ts
+++ src/commands/database-command.ts
@@ -18,7 +18,7 @@ export abstract class DatabaseCommand {
     protected constructor() {
         try {
             const serviceAccount = require(process.env.SERVICE_ACCOUNT);
-            if (DatabaseCommand.APP === undefined && serviceAccount !== {}) {
+            if (DatabaseCommand.APP === undefined && serviceAccount) {
                 DatabaseCommand.APP = admin.initializeApp({
                     credential: admin.credential.cert(serviceAccount),
                     databaseURL: 'https://ffxivteamcraft.firebaseio.com'
