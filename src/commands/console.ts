eba997ef5b88eb1df9104fbaad83cfbba05f6c1b
ArcaneDisgea <magicdisgea@gmail.com>
2020-12-28 08:34:46 -0500

    Add troubleshooting link to console command

--- src/commands/console.ts
+++ src/commands/console.ts
@@ -15,7 +15,7 @@ export class ConsoleCommand implements Command {
     async run(parsedUserCommand: CommandContext): Promise<void> {
         const embed = new MessageEmbed()
         .setTitle("How to open the console")
-        .setDescription("Please open the console and take a screenshot of it to post in #troubleshooting")
+        .setDescription("Please open the console and take a screenshot of it to post in <#639503745176174592>.")
         .addField("Website", "Please press F12 on the keyboard and click the ``console`` tab.")
         .addField("Desktop App", "Please navigate to your Profile > Settings > Desktop > Open Dev Tools and click the ``console`` tab.")
         .addField("I can't get to my profile. How am I supposed to get to the console?", "``CTRL + SHIFT + I`` will also open the dev tools allowing you to still get to the console if the app is not doing anything.")
