7b89de031826853db6d6ec1455b362728745934c
ArcaneDisgea <magicdisgea@gmail.com>
2020-12-28 08:45:25 -0500

    Update dirtyinstall command

--- src/commands/dirty.ts
+++ src/commands/dirty.ts
@@ -1,5 +1,6 @@
 import {Command} from "./command";
 import {CommandContext} from "../models/command_context";
+import { MessageEmbed } from "discord.js";
 
 export class DirtyInstallCommand implements Command {
     commandNames = ["dirtyinstall", "cleanreinstall"];
@@ -12,7 +13,21 @@ export class DirtyInstallCommand implements Command {
     }
 
     async run(parsedUserCommand: CommandContext): Promise<void> {
-        await parsedUserCommand.originalMessage.channel.send("You likely have a dirty install laying around please do the following to fully ensure TC is closed and removed before reinstalling. \n```- Open task manager and end all teamcraft processes\n- Navigate to your appdata folder it is going to be the following path replaced with your windows username C:\\Users\\USERNAME\\AppData\n- Navigate into the roaming folder and please delete the ffxiv-teamcraft folder.\n- Go back to appdata and navigate to the local/Programs folder and delete ffxiv-teamcraft.\n- Please open Control Panel\\Programs\\Programs and Features and make sure teamcraft is not listed in the list of installed programs if it is please click it and click uninstall it will fuss and say that the app cannot be\n  found, just click yes and it should remove it from the list.\n- Restart your computer, this is just in case a lingering instance is holding on to the port TC uses its weird I couldn't tell you why it happens it just does and a restart will fix it.\n- Install Teamcraft from https://ffxivteamcraft.com/desktop```");
+        const embed = new MessageEmbed()
+        .setTitle("Dirty Install Clean-up")
+        .setDescription("You likely have a dirty install of TC laying around. Please do the following to fully ensure TC is closed and removed before reinstalling.")
+        .addField("Close all instances of Teamcraft", "Open your task manager and please end all teamcraft processes.")
+        .addField("Clean up old local files", "Press ``WIN+R`` and type ``%localappdata%``. Locate the ``ffxiv-teamcraft`` folder and delete it. Find it again in the ``programs`` folder and delete it.")
+        .addField("Clean up old roaming files", "Press ``WIN+R`` and type ``%appdata%``. Locate the ``ffxiv-teamcraft`` folder and delete it.")
+        .addField("Uninstall", "Please open ``Control Panel\\Programs\\Programs and Features`` and make sure that TC is not listed. If it is please click it and click uninstall. It might fuss about the app not being able to be found. Ignore this and click ok.")
+        .addField("Restart Computer", "Restart your computer just to be certain that no Teamcraft related processes are running.")
+        .addField("Download and Reinstall", "[Download the latest version of the app and install it.](https://ffxivteamcraft.com/desktop)")
+        .setFooter(
+          "ffxiv-teamcraft",
+          "https://ffxivteamcraft.com/assets/logo.png"
+        )
+        .setColor("#4880b1");
+        await parsedUserCommand.originalMessage.channel.send(embed);
     }
 
     hasPermissionToRun(parsedUserCommand: CommandContext): boolean {
