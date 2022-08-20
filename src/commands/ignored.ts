506d33ec01ad73e394a264f211badf6b6c40dbbf
Venili Barric <79701552+Lalalavicious@users.noreply.github.com>
2022-08-20 20:07:49 +0200

    Updates to pcap and ichara commands
    Added to/edited !!pcap:
    - Mention of Winpcap API-compatible mode
    - Mention of !!ichara
    - Mention of !!vpn generic
    - Changed log location to reflect current version
    
    Added to/edited !!ichara:
    - Mention of needing a TC account to properly link characters
    - Mention of CN client
    - Explicit mention that aethernet teleports don't count

--- src/commands/ignored.ts
+++ src/commands/ignored.ts
@@ -12,7 +12,7 @@ export class IgnoredCommand implements Command {
     }
 
     async run(parsedUserCommand: CommandContext): Promise<void> {
-        let txt = `**To clean up potentially ignored or unlinked characters, please do the following:**\n - Go to Settings, Desktop, Reset ignored characters and Reset linked characters.\n - Restart TC.\n - Go to the Inventory page and click the button to Reset all inventories.\n - In game, sort your inventory. *Make sure at least one item moves.* This should trigger a popup to select your character in TC.\n - Once it's selected, teleport to a new zone. *It has to be a different zone than the one you are currently in -- for example, Limsa to Eulmore.`
+        let txt = `**To clean up potentially ignored or unlinked characters, please do the following:**\n - Note that you must have a Teamcraft account and be logged in to it to ensure that character linking works properly.\n - If you are playing FFXIV using the Chinese client, please ensure Teamcraft is pointed to the correct folder for your character data, as this differs between the Global and China regions.\n - Go to Settings, Desktop, then click the buttons to 'Reset ignored characters' and 'Reset linked characters'.\n - Restart TC.\n - Go to the Inventory page and click the button to Reset all inventories.\n - In game, sort your inventory. *Make sure at least one item moves.* This should trigger a popup to select your character in TC.\n - Once it's selected, teleport to a new zone. *It MUST be an entirely different zone than the one you are currently in -- for example, Limsa to Eulmore -- and will not work if you use aethernet teleports within the same zone.`
         await parsedUserCommand.originalMessage.channel.send(txt);
     }
 
--- src/commands/pcap.ts
+++ src/commands/pcap.ts
@@ -15,15 +15,16 @@ export class PcapCommand implements Command {
     async run(parsedUserCommand: CommandContext): Promise<void> {
         const embed = new MessageEmbed()
         .setTitle("Troubleshooting Packet Capture")
-        .setDescription("Any steps using ``!!commands`` are bot commands meant to be used in <#639503745176174592> for further instructions.\n\n**Attention PS4 Players** Packet capture does not work for the PS4, only on PC.")
+        .setDescription("Any steps using ``!!commands`` are **Discord bot commands** meant to be used in <#639503745176174592> for further instructions.\n\n**Attention PS4/PS5 Players** Packet capture does not work for the PS4/PS5, only on PC.")
         .addField("Is the app updated?", "Teamcraft needs to be on the latest version of the app for packet capture to work. Please check #announcements for the latest version. If you are not up to date, please update before continuing.")
         .addField("Are you using the Desktop App?", "Packet capture only works on the desktop app. Please make sure you have it downloaded and installed before trying to use packet capture.")
         .addField("Have you restarted Teamcraft?", "As of version 8.0.12, PCAP no longer requires a client update to update OPCodes. Restarting Teamcraft can resolve issues related to this.")
         .addField("If the above are true, please do the following.", "Open the ``!!console``, type ``window.debugPackets = true;`` and move in game/teleport to a new zone. Is there any output?")
-        .addField("There was no output! Please help!", "Try to re-install [NPcap](https://nmap.org/npcap/dist/npcap-1.50.exe).")
-        .addField("Still nothing? Are you using a VPN or a tool to lower your ping?", "Go to settings, enable `RawSocket` capture mode, and restart TC as admin.")
+        .addField("There was no output! Please help!", "Try to re-install [NPcap](https://nmap.org/npcap/dist/npcap-1.50.exe). Make sure that 'install in Winpcap API-compatible mode' is enabled.")
+		.addField("What if you *did* get output from ``window.debugPackets = true;``, but your inventory still isn't working right?", "Try the ``!!ichara`` command here in the server, as it's possible you need to reset your character links.")
+        .addField("Still nothing? Are you using a VPN or a tool to lower your ping?", "Try the ``!!vpn generic`` command here in the server to see if we have suggested settings for your specific VPN! Generally, however, go to Settings, enable `RawSocket` capture mode, and restart TC as admin.")
         .addField("Still not working. :(", "Please try a ``!!dirtyinstall`` to make sure that you don't have old files related to Teamcraft laying around mucking things up.")
-        .addField("Still nothing. Am I hopeless?", "The last thing you can do for us to help you is post your ``log.log`` to <#639503745176174592> alongside details regarding your issue. It is located in ``%appdata%/ffxiv-teamcraft``.")
+        .addField("Still nothing. Am I hopeless?", "The last thing you can do for us to help you is post your ``main.log`` to <#639503745176174592> alongside details regarding your issue. It is located in ``%appdata%/ffxiv-teamcraft/logs``.")
         .setFooter(
           "ffxiv-teamcraft",
           "https://ffxivteamcraft.com/assets/logo.png"
