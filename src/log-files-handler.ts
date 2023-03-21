632c53f7b6688ff35c08dc6abb14a289e43af071
Flavien Normand <contact@flavien-normand.fr>
2023-03-21 23:51:02 +0100

    chore(log): better redtick

--- src/log-files-handler.ts
+++ src/log-files-handler.ts
@@ -63,7 +63,7 @@ export class LogFilesHandler {
                     new EmbedBuilder()
                         .setColor(diagnosisColor)
                         .setTitle('Diagnosis')
-                        .setDescription(diagnosis.errors.length === 0 ? ':white_check_mark: No errors detected in the log file.' : diagnosis.errors.map(row => ` :red_circle: ${row}`).join('\n')),
+                        .setDescription(diagnosis.errors.length === 0 ? ':white_check_mark: No errors detected in the log file.' : diagnosis.errors.map(row => ` <:redtick:1087870892790534176> ${row}`).join('\n')),
                     ...(diagnosis.warnings.length > 0 ? [
                         new EmbedBuilder()
                             .setColor('Gold')
